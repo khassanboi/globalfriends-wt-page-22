755efd5a6ad1ebdfa6a30231bd8f40ae1dda9925
khassanboi <khassanboi@gmail.com>
2022-07-15 14:02:25 -0700

    develop: desktop version ready

--- scripts/main.js
+++ scripts/main.js
@@ -14,16 +14,15 @@ $('.owl-carousel').owlCarousel({
 	autoplay: true,
 	autoplayTimeout: 4000,
 	autoplaySpeed: 1000,
-	autoplayHoverPause: true,
 	responsive: {
 		0: {
 			items: 1,
 		},
 		600: {
-			items: 2,
+			items: 3,
 		},
 		1000: {
-			items: 3,
+			items: 4,
 		},
 	},
 });
